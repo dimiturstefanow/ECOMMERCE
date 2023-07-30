f0e88c60c87a419721f5215bd813572524d260e0
dimiturstefanow <dimitrstefanov7v2@gmail.com>
2023-07-30 13:51:25 +0300

    navbar fixed

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ function App() {
   return (
     <>
       <div className="App">
-        <Sidebar />
         <NavbarComponent />
+        <Sidebar />
         <Recommended />
         <Products />
         <FooterComponent />
