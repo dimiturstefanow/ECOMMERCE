6748be4ad3f9bda8f63ad2417683802f86775da7
dimiturstefanow <dimitrstefanov7v2@gmail.com>
2023-07-31 23:16:00 +0300

    have to google, saving this as an reference in case something is broken

--- src/Sidebar/Category/Category.js
+++ src/Sidebar/Category/Category.js
@@ -1,13 +1,16 @@
 import Input from "../../components/Input";
 import "./Category.css";
 
-function Category() {
+function Category({handleChange}) {
   return (
     <div>
       <h2 className="sidebar-title">Category</h2>
 
       <div>
-       <Input />
+       <label className="sidebar-label-container">
+        <input onChange={handleChange} type="radio" value="" name="test" />
+        
+       </label>
       </div>
     </div>
   );
