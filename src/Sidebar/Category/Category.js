93d8c15a5eb62b8540ae2b93d7c7af809af41c1b
dimiturstefanow <dimitrstefanov7v2@gmail.com>
2023-07-31 23:28:34 +0300

    save

--- src/Sidebar/Category/Category.js
+++ src/Sidebar/Category/Category.js
@@ -1,16 +1,40 @@
-import Input from "../../components/Input";
 import "./Category.css";
 
-function Category({handleChange}) {
+function Category({ handleChange }) {
   return (
     <div>
       <h2 className="sidebar-title">Category</h2>
 
       <div>
-       <label className="sidebar-label-container">
-        <input onChange={handleChange} type="radio" value="" name="test" />
-        
-       </label>
+        <label className="sidebar-label-container">
+          <input onChange={handleChange} type="radio" value="" name="test" />
+          <span className="checkmark"></span> All
+        </label>
+
+        <input
+          handleChange={handleChange}
+          value="sneakers"
+          title="Sneakers"
+          name="test"
+        />
+        <input
+          handleChange={handleChange}
+          value="flats"
+          title="Flats"
+          name="test"
+        />
+        <input
+          handleChange={handleChange}
+          value="sandals"
+          title="Sandals"
+          name="test"
+        />
+        <input
+          handleChange={handleChange}
+          value="heels"
+          title="Heels"
+          name="test"
+        />
       </div>
     </div>
   );
