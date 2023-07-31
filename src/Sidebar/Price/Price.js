47522fd7540a3c315d0317acef3b6840592e15bc
dimiturstefanow <dimitrstefanov7v2@gmail.com>
2023-07-31 23:48:34 +0300

    saving again price before major update

--- src/Sidebar/Price/Price.js
+++ src/Sidebar/Price/Price.js
@@ -10,16 +10,16 @@ function Price({ handleChange }) {
         <span className="checkmark"></span> All
       </label>
       <label className="sidebar-label-container">
-        <input onChange={handleChange} type="radio" value="50" name="$0 - $50" />
-        <span className="checkmark"></span> All
+        <input onChange={handleChange} type="radio" value="500" name="test2" />
+        <span className="checkmark"></span> $0 - $50
       </label>
       <label className="sidebar-label-container">
-        <input onChange={handleChange} type="radio" value="100" name="$50 - $100" />
-        <span className="checkmark"></span> All
+        <input onChange={handleChange} type="radio" value="100" name="test2" />
+        <span className="checkmark"></span> $50 - $100
       </label>
       <label className="sidebar-label-container">
-        <input onChange={handleChange} type="radio" value="150" name="$100 - $150" />
-        <span className="checkmark"></span> All
+        <input onChange={handleChange} type="radio" value="150" name="test2" />
+        <span className="checkmark"></span> $100 - $150
       </label>
       <label className="sidebar-label-container">
         <input onChange={handleChange} type="radio" value="200" name="test2" />
