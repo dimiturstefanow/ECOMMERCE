edecc1c237c5799ec8d24bf3b11c71abc4f69a74
dimiturstefanow <dimitrstefanov7v2@gmail.com>
2023-07-31 22:12:05 +0300

    padna mi toka i zabavih procesa

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ import Recommended from "./Recommended/Recommended";
 import Sidebar from "./Sidebar/Sidebar";
 import { useState } from "react";
 import products from "./db/data";
+import { computeHeadingLevel } from "@testing-library/react";
+import { Card } from "react-bootstrap";
 
 function App() {
   const [selectedCategory, setSelectedCategory] = useState(null);
+  const [query, setQuery] = useState("");
 
   // Input Filter
-  const [query, setQuery] = useState("");
 
   const filteredItems = products.filter((product) =>
     product.title.toLocaleLowerCase().indexOf(query.toLocaleLowerCase() !== -1)
@@ -22,12 +24,44 @@ function App() {
     selectedCategory(event.target.value);
   };
 
-  
-
   // Buttons Filter
   const handleClick = (event) => {
     setSelectedCategory(event.target.value);
   };
+
+  function filteredData(products, selected, query) {
+    let filteredProducts = products;
+
+    // Filtering input Items
+    if (query) {
+      filteredProducts = filteredItems;
+    }
+
+    // Selected Filter
+    if (selected) {
+      filteredProducts = filteredProducts.filter(
+        ({ category, color, company, newPrice, title }) =>
+          category === selected ||
+          color === selected ||
+          company === selected ||
+          newPrice === selected ||
+          title === selected
+      );
+    }
+    return filteredProducts.map(({ img, title, star, reviews, newPrice, prevPrice }) => (
+      <Card
+       key={Math.random()} 
+       img={img}
+       title={title}
+       star={star}
+       reviews={reviews}
+       newPrice={newPrice}
+       prevPrice={prevPrice}
+
+
+       />
+    ));
+  }
 
   return (
     <>
