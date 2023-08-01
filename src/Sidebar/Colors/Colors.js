1b8cd9f15765a79c4458b012e1198ab4c18d094d
dimiturstefanow <dimitrstefanov7v2@gmail.com>
2023-08-01 22:57:30 +0300

    colors fixed

--- src/Sidebar/Colors/Colors.js
+++ src/Sidebar/Colors/Colors.js
@@ -1,3 +1,5 @@
+// src\Sidebar\Colors\Colors.js
+
 import Input from "../../components/Input";
 import "./Colors.css";
 
@@ -7,38 +9,43 @@ function Colors({ handleChange }) {
       <h2 className="sidebar-title color-title">Colors</h2>
       <label className="sidebar-label-container">
         <input onChange={handleChange} type="radio" value="" name="test2" />
-        <span className="checkmark all" ></span> All
+        <span className="checkmark all"></span> All
       </label>
-      {/* checkpoint  */}
-      <input handleChange={handleChange} 
-      value="black"
-      title="Black"
-      name="test1"
-      color="black"
+
+      {/* Correct usage: Use `onChange` instead of `handleChange` */}
+      <Input
+        onChange={handleChange}
+        value="black"
+        title="Black"
+        name="test1"
+        color="black"
       />
-      <input handleChange={handleChange} 
-      value="blue"
-      title="Blue"
-      name="test1"
-      color="blue"
+      <Input
+        onChange={handleChange}
+        value="blue"
+        title="Blue"
+        name="test1"
+        color="blue"
       />
-      <input handleChange={handleChange} 
-      value="red"
-      title="Red"
-      name="test1"
-      color="red"
+      <Input
+        onChange={handleChange}
+        value="red"
+        title="Red"
+        name="test1"
+        color="red"
       />
-      <input handleChange={handleChange} 
-      value="green"
-      title="Green"
-      name="test1"
-      color="green"
+      <Input
+        onChange={handleChange}
+        value="green"
+        title="Green"
+        name="test1"
+        color="green"
       />
+
       <label className="sidebar-label-container">
-      <input type="radio" onChange={handleChange} value="white" name="test1"/> 
-      <span className="checkmark" style={{background:"white", border: "2px solid black"}}>
-      
-      </span> White
+        <input type="radio" onChange={handleChange} value="white" name="test1" />
+        <span className="checkmark" style={{ background: "white", border: "2px solid black" }}></span>
+        White
       </label>
     </div>
   );
