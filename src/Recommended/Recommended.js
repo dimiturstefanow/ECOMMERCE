41e7cba17bf8f5d746584ca136bcc0e56c26c436
dimiturstefanow <dimitrstefanov7v2@gmail.com>
2023-08-02 09:22:54 +0300

    again save before recommended major update

--- src/Recommended/Recommended.js
+++ src/Recommended/Recommended.js
@@ -1,15 +1,22 @@
+import { useState } from "react";
 import Buttons from "../components/Buttons";
 import "./Recommended.css";
 
 function Recommended() {
+  const [selectedCategory, setSelectedCategory] = useState("");
+
+  const handleClick = (value) => {
+    setSelectedCategory(value);
+  };
+
   return (
     <>
       <div>
         <h2 className="recommended-title">Recommened</h2>
         <div className="recommended-flex">
-          <button className="btns">All Products</button>
-          <Buttons onClickHandler={handleClick} value="Nike" title="Nike" />
-          <Buttons onClickHandler={handleClick} value="Adidas" title="Adidas" />
+        <Buttons onClickHandler={() => handleClick("Nike")} value="" title="All Products" />
+          <Buttons onClickHandler={() => handleClick("Nike")} value="Nike" title="Nike" />
+          <Buttons onClickHandler={() => handleClick("Adidas")} value="Adidas" title="Adidas" />
         </div>
       </div>
     </>
