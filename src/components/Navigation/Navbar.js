cb9414d2e6fa79390d839adb7722606c8e6bb4da
dimiturstefanow <dimitrstefanov7v2@gmail.com>
2023-08-07 14:07:04 +0300

    add favorite

--- src/components/Navigation/Navbar.js
+++ src/components/Navigation/Navbar.js
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import { Nav, NavDropdown, Navbar } from "react-bootstrap";
 import "./Navbar.css";
 import logo from "../../Images/c4_nexus_logo-removebg-preview.png";
+import data from "../../db/data";
 
 function NavbarComponent({ favorites }) {
+  const [showFav, setFav] = useState(false);
+  const favProducts = data.filter((product) => favorites.includes(product.id));
   const handleLogoClick = () => {
     window.location.reload();
   };
@@ -27,12 +30,31 @@ function NavbarComponent({ favorites }) {
             >
               Home
             </Nav.Link>
-             
-            <Nav.Link href="#favorites" className="navigation-link wrapper-fav-button">
+
+            <Nav.Link
+              href="#favorites"
+              className="navigation-link wrapper-fav-button"
+              onMouseOver={() => setFav(!showFav)}
+              onMouseLeave={() => setFav(!showFav)}
+            >
               Favorites{" "}
               <span className="favorites-button">{favorites.length}</span>
+              <div className="fav-dropdown">
+                <div style={{display: showFav ? "block" : "none"}} className="fav-dropdown-content">
+                  {favProducts.map((product) => 
+                  <div key={product.id}>
+                      <div style={{display: "flex"}}>
+                        <img src={product.img} />
+                        <p>{product.title}</p>
+                      </div>
+                      <div style={{display: "flex", flexDirection: "column"}}>
+                        <p style={{marginLeft: "auto"}}>{product.price}</p>
+                      </div>
+                  </div>)}
+                </div>
+              </div>
             </Nav.Link>
-             
+
             <Nav.Link href="#contact-us" className="navigation-link">
               Contact Us
             </Nav.Link>
